f907cc46957f0d1d8d450220ba7d2cbae4846def
ccbbss11126 <ccbbss11126@gmail.com>
2025-09-09 20:09:07 +0900

    20250909_1830 aJax사용하기

--- src/main/java/com/spring/springGroupS/controller/MessageController.java
+++ src/main/java/com/spring/springGroupS/controller/MessageController.java
@@ -13,7 +13,8 @@ public class MessageController {
 	@RequestMapping(value = "/message/{msgFlag}", method = RequestMethod.GET)
 	public String getMessage(Model model,
 			@PathVariable String msgFlag,
-			@RequestParam(name="mid", defaultValue = "", required = false) String mid
+			@RequestParam(name="mid", defaultValue = "", required = false) String mid,
+			@RequestParam(name="idx", defaultValue = "", required = false) String idx
 		) {
 		
 		if(msgFlag.equals("hoewonInputOk")) {
@@ -24,6 +25,79 @@ public class MessageController {
 			model.addAttribute("message", "회원 가입 실패");
 			model.addAttribute("url", "/study1/mapping/menu");
 		}
+		else if(msgFlag.equals("userInputOk")) {
+			model.addAttribute("message", "회원 가입 성공");
+			model.addAttribute("url", "/user2/userList");
+		}
+		else if(msgFlag.equals("userInputNo")) {
+			model.addAttribute("message", "회원 가입 실패~~");
+			model.addAttribute("url", "/user2/userInput");
+		}
+		else if(msgFlag.equals("userDeleteOk")) {
+			model.addAttribute("message", "회원 삭제 성공");
+			model.addAttribute("url", "/user2/userList");
+		}
+		else if(msgFlag.equals("userDeleteNo")) {
+			model.addAttribute("message", "회원 삭제 실패~~");
+			model.addAttribute("url", "/user2/userList");
+		}
+		else if(msgFlag.equals("userUpdateOk")) {
+			model.addAttribute("message", "회원 정보를 수정하였습니다.");
+			model.addAttribute("url", "/user2/userUpdate?idx="+idx);
+		}
+		else if(msgFlag.equals("userUpdateOk")) {
+			model.addAttribute("message", "회원 정보 수정 실패~~");
+			model.addAttribute("url", "/user2/userUpdate?idx="+idx);
+		}
+		else if(msgFlag.equals("guestInputOk")) {
+			model.addAttribute("message", "방명록에 글이 등록되었습니다.");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestInputNo")) {
+			model.addAttribute("message", "방명록 글등록 실패~~");
+			model.addAttribute("url", "/guest/guestInput");
+		}
+		else if(msgFlag.equals("adminOk")) {
+			model.addAttribute("message", "관리자 인증 성공");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("adminNo")) {
+			model.addAttribute("message", "관리자 인증 실패~~");
+			model.addAttribute("url", "/guest/admin");
+		}
+		else if(msgFlag.equals("adminOut")) {
+			model.addAttribute("message", "관리자 로그아웃");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestDeleteOk")) {
+			model.addAttribute("message", "방명록 게시글이 삭제되었습니다.");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestDeleteNo")) {
+			model.addAttribute("message", "방명록 게시글 삭제실패~~");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("memberJoinOk")) {
+			model.addAttribute("msg", "회원에 가입되셨습니다.");
+			model.addAttribute("url", "/member/memberLogin");
+		}
+		else if(msgFlag.equals("memberJoinNo")) {
+			model.addAttribute("msg", "회원 가입 실패~~");
+			model.addAttribute("url", "/member/memberJoin");
+		}
+		else if(msgFlag.equals("memberLoginOk")) {
+			model.addAttribute("msg", mid+"님 로그인 되셨습니다.");
+			model.addAttribute("url", "/member/memberMain");
+		}
+		else if(msgFlag.equals("memberLoginNo")) {
+			model.addAttribute("msg", "로그인 실패~~");
+			model.addAttribute("url", "/member/memberLogin");
+		}
+		else if(msgFlag.equals("memberLogout")) {
+			model.addAttribute("msg", mid + "님 로그아웃 되셨습니다.");
+			model.addAttribute("url", "/member/memberLogin");
+		}
+
 		
 		return "include/message";
 	}
--- src/main/webapp/WEB-INF/views/include/nav.jsp
+++ src/main/webapp/WEB-INF/views/include/nav.jsp
@@ -7,7 +7,7 @@
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="http://192.168.50.20:9090/springGroupS/" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-    <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Guest</a>
+    <a href="${ctp}/guest/guestList" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Guest</a>
     <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Board</a>
     <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PDS</a>
     <div class="w3-dropdown-hover w3-hide-small">
@@ -16,11 +16,15 @@
         <a href="${ctp}/study1/mapping/menu" class="w3-bar-item w3-button">Mapping</a>
         <a href="${ctp}/study1/aop/aopMenu" class="w3-bar-item w3-button">AOP Test</a>
         <a href="${ctp}/study1/xml/xmlMenu" class="w3-bar-item w3-button">XML 값주입 Test</a>
-        <a href="#" class="w3-bar-item w3-button">Ajax Test</a>
+        <a href="${ctp}/user/userList" class="w3-bar-item w3-button">User List</a>
+        <a href="${ctp}/user2/userList" class="w3-bar-item w3-button">User2 List</a>
+        <a href="${ctp}/study1/restApi/restApiForm" class="w3-bar-item w3-button">Rest API</a>
+        <a href="${ctp}/study1/ajax/ajaxForm" class="w3-bar-item w3-button">Ajax Test</a>
+        <a href="${ctp}/study1/password/passwordForm" class="w3-bar-item w3-button">암호화연습</a>
       </div>
     </div>
     <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Login</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Join</a>
+    <a href="${ctp}/member/memberJoin" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Join</a>
     <!-- <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a> -->
   </div>
 </div>
